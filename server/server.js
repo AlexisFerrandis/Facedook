0fd20ad5689c105296cb85bbc40f09d86c0d1b5a
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-06-24 10:08:41 +0200

    User CRUD

--- server/server.js
+++ server/server.js
@@ -1,41 +1,41 @@
 const express = require("express");
-// const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 
-// const userRoutes = require("./routes/user.routes");
+const userRoutes = require("./routes/user.routes");
 // const postRoutes = require("./routes/post.routes");
 
-// const { checkUser, requireAuth } = require("./middlewares/auth.middleware");
+const { checkUser, requireAuth } = require("./middlewares/auth.middleware");
 
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// const corsOptions = {
-// 	origin: process.env.CLIENT_URL,
-// 	credentials: true,
-// 	allowedHeaders: ["sessionId", "Content-Type"],
-// 	exposedHeaders: ["sessionId"],
-// 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-// 	preflightContinue: false,
-// };
+const corsOptions = {
+	origin: process.env.CLIENT_URL,
+	credentials: true,
+	allowedHeaders: ["sessionId", "Content-Type"],
+	exposedHeaders: ["sessionId"],
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	preflightContinue: false,
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
+app.use(cookieParser());
 
-// jwt
-// app.get("*", checkUser);
-// app.get("/jwtid", requireAuth, (req, res) => {
-// 	res.status(200).send(res.locals.user._id);
-// });
+// jwt;
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+	res.status(200).send(res.locals.user._id);
+});
 
 //routes
-// app.use("/api/user", userRoutes);
+app.use("/api/user", userRoutes);
 // app.use("/api/post", postRoutes);
 
 // server
