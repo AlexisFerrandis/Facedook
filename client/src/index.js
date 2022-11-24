1b1dcef0ce657dc907e333bc0d56e5397f7df402
unknown <alexis.ferrandis@gmail.com>
2022-11-24 08:51:33 +0100

    Dependencies correction

--- client/src/index.js
+++ client/src/index.js
@@ -20,9 +20,9 @@ store.dispatch(getUsers());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<Provider store={store}>
-		<React.StrictMode>
+		{/* <React.StrictMode> */}
 			<App />
-		</React.StrictMode>
+		{/* </React.StrictMode> */}
 	</Provider>
 );
 
